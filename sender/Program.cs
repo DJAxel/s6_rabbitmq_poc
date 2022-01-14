c4b20462f6590c23cce22dbf79840c1be93b9317
Axel Köhler <contact@axelkohler.nl>
2022-01-14 11:06:31 +0100

    Send a message every second
    Create a thread that sends a message with the current time every second
    until the program is terminated.

--- sender/Program.cs
+++ sender/Program.cs
@@ -1,35 +1,23 @@
 ﻿using System;
 using RabbitMQ.Client;
 using System.Text;
+using System.Threading;
 
 namespace sender
 {
     class Program
     {
+        
+
         static void Main(string[] args)
         {
-            var factory = new ConnectionFactory() { HostName = "localhost" };
-            using (var connection = factory.CreateConnection())
-            using (var channel = connection.CreateModel())
-            {
-                channel.QueueDeclare(queue: "hello",
-                                 durable: false,
-                                 exclusive: false,
-                                 autoDelete: false,
-                                 arguments: null);
+            MessageSender messageSender = MessageSender.Instance;
+            messageSender.startSending();
 
-                string message = "Hello World!";
-                var body = Encoding.UTF8.GetBytes(message);
-
-                channel.BasicPublish(exchange: "",
-                                     routingKey: "hello",
-                                     basicProperties: null,
-                                     body: body);
-                Console.WriteLine(" [x] Sent {0}", message);
-            }
-            
             Console.WriteLine(" Press [enter] to exit.");
+            Console.WriteLine("");
             Console.ReadLine();
+            messageSender.stopSending();
         }
     }
 }
